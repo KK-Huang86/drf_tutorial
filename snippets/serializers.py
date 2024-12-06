676ca2e7900ed68b2e035ef2a8582137d7140659
Kk <iankk0806@gmail.com>
2024-12-06 23:07:32 +0800

    12_6_drf

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,25 +1,24 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-    def create(self, validated_data):
-      #透過驗證實現的實例
-        return Snippet.objects.create(**validated_data)
+  # ModelSerializer已有默認的 create 與 update的行為，除非有特殊邏輯與型態
 
-    def update(self, instance, validated_data):
-      #更新實現的實例
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+    # def create(self, validated_data):
+    #   #透過驗證實現的實例
+    #     return Snippet.objects.create(**validated_data)
+
+    # def update(self, instance, validated_data):
+    #   #更新實現的實例
+    #     instance.title = validated_data.get('title', instance.title) #若前端沒有資料，則使用原來的instance.title
+    #     instance.code = validated_data.get('code', instance.code)
+    #     instance.linenos = validated_data.get('linenos', instance.linenos)
+    #     instance.language = validated_data.get('language', instance.language)
+    #     instance.style = validated_data.get('style', instance.style)
+    #     instance.save()
+    #     return instance
---
+++ snippets/urls.py
@@ -0,0 +1,11 @@
+from snippets import views
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),  
+    ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
